04723fe4a5ed2890fb4855f167037c2e1f81c1f6
Johnny <johnstuartgonsales@gmail.com>
2022-12-05 15:08:06 +0000

    removed single line

--- edmund.py
+++ edmund.py
@@ -100,7 +100,6 @@ async def higurashi(ctx):
 async def feeling(ctx):
     await ctx.send(random.choice(moods))
 
-
 #id thing
 with open(os.path.join(sys.path[0], "token.txt"), "r", encoding='utf8') as f:
     TOKEN = f.readline()
